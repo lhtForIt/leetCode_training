6aff39bacc549a42ad904f9fd3986248e3419776
Leo <lhtshen@163.com>
2021-04-05 23:40:38 +0800

    nine week，fighting

---
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,89 @@
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。 
+//
+// 返回同样按升序排列的结果链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序排列 
+// 
+// Related Topics 链表 
+// 👍 552 👎 0
+
+package leetcode.editor.cn;
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        /**
+         * 迭代
+         */
+//        if (head == null) {
+//            return head;
+//        }
+//        ListNode cur = head;
+//        while (cur != null && cur.next != null) {
+//            if (cur.val == cur.next.val) {
+//                //将指针指向下下个节点
+//                cur.next = cur.next.next;
+//            } else {
+//                //直接指针后移
+//                cur = cur.next;
+//            }
+//        }
+//
+//        return head;
+
+        /**
+         * 递归
+         */
+
+        if(head == null || head.next == null)return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+}
---
+++ src/main/java/leetcode/editor/cn/ValidPalindromeIi.java
@@ -0,0 +1,68 @@
+//给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。 
+//
+// 示例 1: 
+//
+// 
+//输入: "aba"
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "abca"
+//输出: True
+//解释: 你可以删除c字符。
+// 
+//
+// 注意: 
+//
+// 
+// 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。 
+// 
+// Related Topics 字符串 
+// 👍 336 👎 0
+
+package leetcode.editor.cn;
+public class ValidPalindromeIi {
+    public static void main(String[] args) {
+        Solution solution = new ValidPalindromeIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        /**
+         * 贪心算法，双指针
+         */
+        int low = 0, high = s.length() - 1;
+        while (low < high) {
+            char c1 = s.charAt(low), c2 = s.charAt(high);
+            if (c1 == c2) {
+                ++low;
+                --high;
+            } else {
+                return validPalindrome(s, low, high - 1) || validPalindrome(s, low + 1, high);
+            }
+        }
+        return true;
+
+
+
+
+
+    }
+
+    private boolean validPalindrome(String s, int low, int high) {
+        for (int i = low, j = high; i < j; ++i, --j) {
+            char c1 = s.charAt(i), c2 = s.charAt(j);
+            if (c1 != c2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
